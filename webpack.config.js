9b03477e5b0ea8615081b26c9035f6dfad95922b
Mayank Mishra <maymishraank@gmail.com>
2022-02-18 19:51:54 +0530

    Added css & sass loaders

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,22 @@
 const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   module: {
     rules: [
+      {
+        test: /\.s?css$/i,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              publicPath: "",
+            },
+          },
+          "css-loader",
+          "sass-loader",
+        ],
+      },
       // {
       //   test: /\.js$/,
       //   exclude: /node_modules/,
@@ -13,9 +27,16 @@ module.exports = {
     ],
   },
 
+  plugins: [new MiniCssExtractPlugin()],
+
   //devTool: "source-maps",
 
   devServer: {
     static: "./dist",
+    hot: true,
+    client: {
+      logging: "none",
+    },
+    port: 3000,
   },
 };
